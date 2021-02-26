74665df2bacf00192f3de04b4de932bcd90ef226
RafaelKozar <rafael_kozar@hotmail.com>
2021-02-26 15:45:03 -0300

    Component Countdown

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import styles from '../styles/components/Countdown.module.css';
+
+export function Countdown() {
+    const [time, setTime] = useState(25*60);
+    const [active, setActive] = useState(false);
+
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    const [minuteLeft, minutRight] = String(minutes).padStart(2, '0').split('');
+    const [secondeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+    function startCountDonwn()
+    {
+        setActive(true);
+    }
+
+    useEffect(() => {
+        if(active && time > 0)
+        {
+            setTimeout(() => {
+                setTime(time-1);
+            }, 1000);
+        }
+    }, [active, time]);
+
+    return (
+        <div>
+            <div className={styles.countdownContainer}>
+                <div>
+                    <span>{minuteLeft}</span>
+                    <span>{minutRight}</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>{secondeft}</span>
+                    <span>{secondRight}</span>
+                </div>
+            </div>
+            <button type="button" onClick={startCountDonwn} className={styles.countdownButton} >
+                Iniciar um ciclo
+            </button>
+        </div>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import CompletedChallenges from "../components/CompletedChallenges";
+import {Countdown} from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import styles from "../styles/pages/Home.module.css";
 
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | Mova</title>
+      </Head>
+
       <ExperienceBar />
       <section >
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
           </div>
       </section>
     </div>
